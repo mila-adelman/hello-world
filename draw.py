6f372b1f60d59dbb0540d3df1d48e19fcc364a45
Mila Adelman <mila.adelman@gmail.com>
2019-06-20 12:47:52 -0400

    added two python scripts

---
+++ draw.py
@@ -0,0 +1,8 @@
+import turtle
+painter = turtle.Turtle()
+
+for _ in range (4):
+	ainter.forward(100)
+	painter.left(90)
+
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,10 @@
+import turtle
+painter = turtle.Turtle()
+painter.pensize(20)
+
+
+for _ in range (18):
+	ainter.forward(100)
+	painter.left(90)
+
+turtle.done()
